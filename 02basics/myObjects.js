73559efa90ccec5be36560bfa4faa9e1bd935602
CalvinParkNov <dev.calvinpark@gmail.com>
2022-12-17 20:07:44 +0900

    feat: readonly

--- 02basics/myObjects.js
+++ 02basics/myObjects.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 var User = {
     name: "calvin",
     email: "calvinpark@gmail.com",
@@ -7,4 +9,33 @@ var User = {
 function createUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-createUser();
+createUser({
+    name: "calvin",
+    isPaid: false
+});
+// bad syntax
+var newUser = { name: "calvin", isPaid: false, email: "something@gmail.com" };
+// it is a bad syntax because it pass more then function createUser
+// also this is not typescript syntax.
+createUser(newUser);
+// Exactly the same as the easrlier example
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 100, y: 100 });
+function createUser1(user) {
+    return { name: "", email: "", isActive: true };
+}
+createUser1({ name: "", email: "", isActive: true });
+// this is a function parameter, return type, and definition
+function createCourse() {
+    return { name: "typescript", price: 400 };
+}
+var myUser = {
+    _id: "1234",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+};
+myUser.email = "h@gmail.com";
--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -18,11 +18,54 @@ let newUser = { name: "calvin", isPaid: false, email: "something@gmail.com" };
 // also this is not typescript syntax.
 createUser(newUser);
 
-// to fix it
+// to fix it type alias
+// below type is very common thing to do in typescript
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
+//official docs
+type Point = {
+  x: number;
+  y: number;
+};
+
+// Exactly the same as the easrlier example
+function printCoord(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 100, y: 100 });
+
+function createUser1(user: User): User {
+  return { name: "", email: "", isActive: true };
+}
+createUser1({ name: "", email: "", isActive: true });
 // this is a function parameter, return type, and definition
 function createCourse(): { name: string; price: number } {
   return { name: "typescript", price: 400 };
 }
+
+//if you are using mongoDB
+// readonly can use in interface and type.
+// readonly can declare but changeable.
+type User1 = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  creditcardDetails?: number;
+};
+
+let myUser: User1 = {
+  _id: "1234",
+  name: "h",
+  email: "h@h.com",
+  isActive: false,
+};
+
+myUser.email = "h@gmail.com";
 
 export {};
